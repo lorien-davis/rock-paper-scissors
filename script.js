35ead8ce354cb0b2a4bf65c2f1e46c76f2adcd2c
Lorien <lozid123@gmail.com>
2021-10-02 06:46:13 +1000

    Add game functionality

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const playerIcon = document.querySelector("#player");
+const opponentIcon = document.querySelector("#opponent");
+const resultText = document.querySelector(".results");
+const buttons = document.querySelectorAll("div.button");
+
 const getOpponentSelection = () => {
   const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * 3)];
@@ -14,48 +19,37 @@ const checkPlayerWins = (player, opponent) => {
   }
 };
 
-const computeRound = (player, opponent) => {
+const getRoundResults = (player, opponent) => {
   if (player === opponent) return "tie";
 
   return checkPlayerWins(player, opponent) ? "win" : "lose";
 };
 
-const printResults = (player, opponent, result, round) => {
-  console.log(`---ROUND ${round}---`);
+const printResults = (player, opponent, result) => {
   console.log(`You: ${player}\nComputer: ${opponent} \nResult: ${result}`);
 };
 
-const playRound = () => {
+const computeRound = (player) => {
   const opponent = getOpponentSelection();
-  const player = prompt("Input your selection").toLowerCase();
-  const result = computeRound(player, opponent);
+  const result = getRoundResults(player, opponent);
 
   return { player, opponent, result };
 };
 
-const playGame = () => {
-  const score = { player: 0, computer: 0 };
-  let rounds = 3;
-
-  for (let i = 1; i <= rounds; i += 1) {
-    const round = playRound();
-
-    switch (round.result) {
-      case "win":
-        score.player += 1;
-        break;
-      case "lose":
-        score.computer += 1;
-        break;
-      default:
-        rounds += 1;
-        break;
-    }
-
-    printResults(round.player, round.opponent, round.result, i);
-  }
-
-  score.player > score.computer
-    ? console.log(`You won by ${score.player - score.computer}!`)
-    : console.log(`You lost by ${score.computer - score.player}!`);
+const changeIcon = (icon, selection) => {
+  icon.src = `./img/${selection}.png`;
 };
+
+const changeText = (textElement, newText) => {
+  textElement.textContent = newText;
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const roundInfo = computeRound(e.currentTarget.id);
+
+    changeIcon(playerIcon, roundInfo.player);
+    changeIcon(opponentIcon, roundInfo.opponent);
+    changeText(resultText, roundInfo.result);
+  });
+});
